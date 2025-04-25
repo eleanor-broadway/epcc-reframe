bd03e412cdce8a619b67ee61e813a7deaf03c0fd
JPRichings <j.richings@epcc.ed.ac.uk>
2025-04-25 14:30:08 +0100

    update openfoam fetch test

---
+++ tests/apps/openfoam/Fetch_OpenFoam.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+"""Reframe test for OpenFoam"""
+
+# Based on original work from:
+#   Copyright 2016-2022 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
+#   ReFrame Project Developers. See the top-level LICENSE file for details.
+#   SPDX-License-Identifier: BSD-3-Clause
+
+import os
+import reframe as rfm
+import reframe.utility.sanity as sn
+import reframe.utility.udeps as udeps
+
+
+@rfm.simple_test
+class fetch_OpenFoam(rfm.RunOnlyRegressionTest):
+    """Downlaod OpenFoam"""
+    version = variable(str, value="v2212")
+    executable = 'wget'
+    executable_opts = [
+        f"https://sourceforge.net/projects/openfoam/files/{version}/OpenFOAM-{version}.tgz",
+        f"https://sourceforge.net/projects/openfoam/files/{version}/ThirdParty-{version}.tgz"
+    ]
+    local = True
+    valid_systems = ['archer2:login']
+    valid_prog_environs = ['PrgEnv-gnu']
+
+    tags = {"fetch"}
+
+    @sanity_function
+    def validate_download(self):
+        return (sn.path_isfile(f"ThirdParty-{self.version}.tgz") and sn.path_isfile(f"OpenFOAM-{self.version}.tgz"))
