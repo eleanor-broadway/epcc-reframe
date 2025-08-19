3bdb033908eab6994bee5390c9c76b58fa2edc77
Eleanor Broadway <eleanor@EPCC-MBP-P12-245.local>
2025-08-19 14:39:40 +0100

    fixes indents + adds num_nodes

--- tests/pytorch/test.py
+++ tests/pytorch/test.py
@@ -4,11 +4,11 @@ import reframe as rfm
 import reframe.utility.sanity as sn
 
 class PyTorchBaseEnvironment(rfm.RunOnlyRegressionTest): 
-        maintainers = ["e.broadway@epcc.ed.ac.uk"]
-        strict_check = True
-        use_multithreading = False
-        valid_systems = ["archer2:compute-gpu"]
-        valid_prog_environs = ["PrgEnv-cray"]
+    maintainers = ["e.broadway@epcc.ed.ac.uk"]
+    strict_check = True
+    use_multithreading = False
+    valid_systems = ["archer2:compute-gpu"]
+    valid_prog_environs = ["PrgEnv-cray"]
 
     @sanity_function
     def assert_finished(self):
@@ -45,4 +45,24 @@ class DeepCAMARCHER2GPUBase(PyTorchBaseEnvironment):
                 "--local_batch_size 8",
                 "--max_epochs 5"
             ]
+
+    @run_before("run")
+    def set_task_distribution(self):
+        """Setup task distribution"""
+        if self.num_gpus <= 4:
+            self.num_nodes = 1
+        else:
+            if self.num_gpus / 4 - float(self.num_gpus // 4) == 0:
+                self.num_nodes = self.num_gpus // 4
+            else:
+                self.num_nodes = self.num_nodes // 4 + 1
+
+        if self.current_system.name in ["cirrus"]:
+            self.job.options = [
+                f"--nodes={self.num_nodes}",
+                "--exclusive",
+                f"--gres=gpu:{self.num_gpus if self.num_gpus <= 4 else 4}",
+            ]  # make sure you change ntasks in PARAMS
+        elif self.current_system.name in ["archer2"]:
             self.job.options = [f"--nodes={self.num_nodes}", "--exclusive"]
+
