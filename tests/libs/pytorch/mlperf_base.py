5a88d6b839b75e5d396d9d924596f90ee2ca9267
Eleanor Broadway <eleanor@EPCC-MBP-P12-245.local>
2025-08-25 14:43:38 +0100

    fixing with flake8 and pylint

--- tests/libs/pytorch/mlperf_base.py
+++ tests/libs/pytorch/mlperf_base.py
@@ -1,24 +1,29 @@
-import os
+"""Setting base functions for MLPerf HPC tests"""
+
 import reframe as rfm
 import reframe.utility.sanity as sn
-import reframe.utility.udeps as udeps
 
 from mlperf_build import fetch_mlperf_hpc_benchmarks
 from mlperf_build import BuildMLPerfPytorchEnv
 
+
 class MLPerfBase(rfm.RunOnlyRegressionTest):
+    """Base module for MLPerf tests"""
     executable = "python"
     mlperf_benchmarks = fixture(fetch_mlperf_hpc_benchmarks, scope='session')
 
-class DeepCAMBase(MLPerfBase):
 
+class DeepCAMBase(MLPerfBase):
+    """Base module for DeepCAM MLPerf tests"""
     pytorch_env = fixture(BuildMLPerfPytorchEnv, scope='environment')
-    
+
     @performance_function("s", perf_key="epoch-time")
-    def extract_time(self): 
-        return sn.extractsingle(r'Time For Epoch 5 : (\S+) s',
+    def extract_time(self):
+        """Return time for epoch 5"""
+        return sn.extractsingle(
+            r'Time For Epoch 5 : (\S+) s',
             self.stdout,
-            tag=1, 
+            tag=1,
             conv=float,
         )
 
