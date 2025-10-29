638e063c5c18253699c271a56d35899cf95863a8
Eleanor Broadway <eleanor@EPCC-MBP-P12-245.local>
2025-10-29 17:25:44 +0000

    First pass

---
+++ tests/apps/fhiaims/fhiaims_small.py
@@ -0,0 +1,27 @@
+"""ReFrame Tests for FHI-aims"""
+
+import reframe as rfm
+from fhiaims_base import FHIaimsBase
+
+class FHIaimsAcLysAla19HBenchmark(FHIaimsBase):
+    """Base class to run the AUSURF112 QE Smoke test on ARCHER2"""
+
+    n_nodes = 1
+    num_tasks = 128
+    num_tasks_per_node = 128
+    num_cpus_per_task = 1
+    time_limit = "20m"
+    env_vars = {"OMP_NUM_THREADS": str(num_cpus_per_task)}
+    extra_resources = {"qos": {"qos": "standard"}}
+
+    if FHIaimsBase.fhiaims_version == "240920.0":
+        reference = {"archer2:compute": {"PWSCF_wall": (260.0, -0.1, 0.1, "s")}}
+
+@rfm.simple_test
+class FHIaimsAcLysAla19HBenchmarkModule(FHIaimsAcLysAla19HBenchmark):
+    
+    tags = {"applications", "performance"}
+    executable = "aims.mpi.x"
+    modules = [f"fhiaims/{QEBaseEnvironment.qe_version}"]
+
+
