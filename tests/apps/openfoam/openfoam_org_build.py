f949868cd44ec96e9069282555fe825259ad1b3a
Eleanor Broadway <eleanor@EPCC-MBP-P12-245.local>
2025-10-30 13:33:14 +0000

    Debugging cp source code

--- tests/apps/openfoam/openfoam_org_build.py
+++ tests/apps/openfoam/openfoam_org_build.py
@@ -60,21 +60,29 @@ class CompileOpenFOAM(rfm.CompileOnlyRegressionTest):
     time_limit = "4h"
 
     @run_before("compile")
-    def prepare_and_build(self):
+    def copy_sources(self):
         """Copy tarballs from FetchOpenFOAM output."""
         self.prerun_cmds = [
             f"cp {self.openfoam_src.stagedir}/OpenFOAM-*.tar.gz .",
             f"cp {self.openfoam_src.stagedir}/ThirdParty-*.tar.gz ."
-            f"tar xf OpenFOAM-{OpenFOAMBase.openfoam_org_version_major}-{OpenFOAMBase.openfoam_org_version_patch}.tar.gz",
-            f"tar xf ThirdParty-{OpenFOAMBase.openfoam_org_version_major}-version-{OpenFOAMBase.openfoam_org_version_major}.tar.gz", 
-            f"mv OpenFOAM-{OpenFOAMBase.openfoam_org_version_major}-{OpenFOAMBase.openfoam_org_version_patch} OpenFOAM-{OpenFOAMBase.openfoam_org_version_major}",
-            f"mv ThirdParty-{OpenFOAMBase.openfoam_org_version_major}-version-{OpenFOAMBase.openfoam_org_version_major} ThirdParty-{OpenFOAMBase.openfoam_org_version_major}"
         ]
 
-        # self.build_system.commands = [
-        #     'chmod u+x site/compile.sh',
-        #     './site/compile.sh',
-        # ]
+    # @run_before("compile")
+    # def prepare_and_build(self):
+    #     """Copy tarballs from FetchOpenFOAM output."""
+    #     self.prerun_cmds = [
+    #         f"cp {self.openfoam_src.stagedir}/OpenFOAM-*.tar.gz .",
+    #         f"cp {self.openfoam_src.stagedir}/ThirdParty-*.tar.gz .",
+    #         f"tar xf OpenFOAM-{OpenFOAMBase.openfoam_org_version_major}-{OpenFOAMBase.openfoam_org_version_patch}.tar.gz",
+    #         f"tar xf ThirdParty-{OpenFOAMBase.openfoam_org_version_major}-version-{OpenFOAMBase.openfoam_org_version_major}.tar.gz", 
+    #         f"mv OpenFOAM-{OpenFOAMBase.openfoam_org_version_major}-{OpenFOAMBase.openfoam_org_version_patch} OpenFOAM-{OpenFOAMBase.openfoam_org_version_major}",
+    #         f"mv ThirdParty-{OpenFOAMBase.openfoam_org_version_major}-version-{OpenFOAMBase.openfoam_org_version_major} ThirdParty-{OpenFOAMBase.openfoam_org_version_major}"
+    #     ]
+
+    #     self.build_system.commands = [
+    #         'chmod u+x site/compile.sh',
+    #         './site/compile.sh',
+    #     ]
 
 
 
