70efbe0f0514f67853e5ad4cc8e712ddaaa015a8
Eleanor Broadway <eleanor@EPCC-MBP-P12-245.local>
2025-10-30 12:13:27 +0000

    Adding phase 2 build - first attempt

--- tests/apps/openfoam/openfoam_org_build.py
+++ tests/apps/openfoam/openfoam_org_build.py
@@ -10,12 +10,13 @@ import reframe as rfm
 import reframe.utility.sanity as sn
 from openfoam_org_base import OpenFOAMBase
 
-@rfm.simple_test
-class FetchOpenFoam(OpenFOAMBase):
+class FetchOpenFOAM(rfm.RunOnlyRegressionTest):
     """Download OpenFoam"""
 
+    local = True
+    tags = {"fetch"}
+
     valid_systems = ["archer2:login"]
-    # version = f"{OpenFOAMBase.openfoam_org_version}"
     executable = "bash"
     executable_opts = [
         "-c",
@@ -26,8 +27,6 @@ class FetchOpenFoam(OpenFOAMBase):
         http://dl.openfoam.org/third-party/{OpenFOAMBase.openfoam_org_version_major}
         """ 
     ]
-    local = True
-    tags = {"fetch"}
 
     @sanity_function
     def validate_download(self):
@@ -37,74 +36,103 @@ class FetchOpenFoam(OpenFOAMBase):
             sn.path_isfile(f"ThirdParty-{OpenFOAMBase.openfoam_org_version_major}-version-{OpenFOAMBase.openfoam_org_version_major}.tar.gz")
         ])
 
-# @rfm.simple_test
+
+@rfm.simple_test
 class CompileOpenFoam(rfm.CompileOnlyRegressionTest):
     """Test compilation of OpenFoam"""
 
-    build_system = "Make"
-    fetch_openfoam = fixture(FetchOpenFoam, scope="environment")
-
     valid_systems = ["archer2:compute"]
     valid_prog_environs = ["PrgEnv-gnu"]
-
     tags = {"compile"}
 
-    modules = ["gcc/11.2.0", "mkl", "cray-fftw"]
+    # local = True
+    # build_locally = True
 
-    build_prefix = ""
+    build_system = "SingleSource"
 
-    env_vars = {"FOAM_VERBOSE": "1", "FFTW_ARCH_PATH": "${FFTW_DIR}"}
 
+    openfoam_source = fixture(FetchOpenFOAM, scope="environment")
+
+    @run_before("compile")
+    def copy_sources(self):
+        """Copy tarballs from FetchOpenFOAM output."""
+        self.prerun_cmds = [
+            f"cp {self.openfoam_src.stagedir}/OpenFOAM-*.tar.gz .",
+            f"cp {self.openfoam_src.stagedir}/ThirdParty-*.tar.gz ."
+        ]
+    
     num_nodes = 1
     num_tasks_per_node = 1
     num_cpus_per_task = 1
     num_tasks = num_nodes * num_tasks_per_node * num_cpus_per_task
     time_limit = "4h"
 
-    @run_before("compile")
-    def prepare_build(self):
-        """Prepare environment for build"""
-        version = "v2412"
-        label1 = "OpenFOAM"
-        name1 = f"{label1}-{version}"
-        archive1 = f"{label1}-{version}.tgz"
-        label2 = "ThirdParty"
-        name2 = f"{label2}-{version}"
-        archive2 = f"{label2}-{version}.tgz"
-
-        self.build_prefix = f"{name1}"
-
-        config_file_fftw = f"{name1}/etc/config.sh/FFTW"
-        config_file_paraview = f"{name1}/etc/config.sh/paraview"
-
-        fullpath1 = os.path.join(self.fetch_openfoam.stagedir, archive1)
-        fullpath2 = os.path.join(self.fetch_openfoam.stagedir, archive2)
-
-        self.prebuild_cmds = [
-            f"cp {fullpath1} {self.stagedir}",
-            f"cp {fullpath2} {self.stagedir}",
-            f"tar xzf {archive1}",
-            f"tar xzf {archive2}",
-            f"export FOAM_SRC={self.build_prefix}",
-            f"export FOAM_THIRDPARTY={name2}",
-            f"cp prefs.sh {name1}/etc/prefs.sh",
-            f"sed -i 's/^fftw_version.*/fftw_version=fftw-system/' {config_file_fftw}",
-            f"sed -i 's/^export FFTW_ARCH_PATH.*//' {config_file_fftw}",
-            f"sed -i 's/^ParaView_VERSION.*/ParaView_VERSION=none/' {config_file_paraview}",
-            f"cd {self.build_prefix}",
-            "pwd",
-            "ls",
-            "module list",
-            "source ./etc/bashrc",
-            "echo $WM_PROJECT_DIR",
-            "ls $WM_PROJECT_DIR",
-            "./Allwmake -j 16",
-            "./Allwmake -j 1",
-        ]
-
-        self.build_system.options = ["-v", "-n"]
+    executable = 'source'
+    executable_opts = [f'./site-v{OpenFOAMBase.openfoam_org_version_major}.{OpenFOAMBase.openfoam_org_version_patch}/compile.sh']
 
     @sanity_function
-    def validate_compile(self):
-        """Validate compilation by checking existance of binary"""
-        return sn.assert_eq(0, 0)
+        def validate_build(self):
+            """Check that build completed successfully."""
+            return sn.assert_found(r"Build complete", self.stdout)
+
+
+
+
+
+
+
+    # env_vars = {"FOAM_VERBOSE": "1", "FFTW_ARCH_PATH": "${FFTW_DIR}"}
+
+    # num_nodes = 1
+    # num_tasks_per_node = 1
+    # num_cpus_per_task = 1
+    # num_tasks = num_nodes * num_tasks_per_node * num_cpus_per_task
+    # time_limit = "4h"
+
+    # @run_before("compile")
+    # def prepare_build(self):
+    #     """Prepare environment for build"""
+    #     version = "v2412"
+    #     label1 = "OpenFOAM"
+    #     name1 = f"{label1}-{version}"
+    #     archive1 = f"{label1}-{version}.tgz"
+    #     label2 = "ThirdParty"
+    #     name2 = f"{label2}-{version}"
+    #     archive2 = f"{label2}-{version}.tgz"
+
+    #     self.build_prefix = f"{name1}"
+
+    #     config_file_fftw = f"{name1}/etc/config.sh/FFTW"
+    #     config_file_paraview = f"{name1}/etc/config.sh/paraview"
+
+    #     fullpath1 = os.path.join(self.fetch_openfoam.stagedir, archive1)
+    #     fullpath2 = os.path.join(self.fetch_openfoam.stagedir, archive2)
+
+    #     self.prebuild_cmds = [
+    #         f"cp {fullpath1} {self.stagedir}",
+    #         f"cp {fullpath2} {self.stagedir}",
+    #         f"tar xzf {archive1}",
+    #         f"tar xzf {archive2}",
+    #         f"export FOAM_SRC={self.build_prefix}",
+    #         f"export FOAM_THIRDPARTY={name2}",
+    #         f"cp prefs.sh {name1}/etc/prefs.sh",
+    #         f"sed -i 's/^fftw_version.*/fftw_version=fftw-system/' {config_file_fftw}",
+    #         f"sed -i 's/^export FFTW_ARCH_PATH.*//' {config_file_fftw}",
+    #         f"sed -i 's/^ParaView_VERSION.*/ParaView_VERSION=none/' {config_file_paraview}",
+    #         f"cd {self.build_prefix}",
+    #         "pwd",
+    #         "ls",
+    #         "module list",
+    #         "source ./etc/bashrc",
+    #         "echo $WM_PROJECT_DIR",
+    #         "ls $WM_PROJECT_DIR",
+    #         "./Allwmake -j 16",
+    #         "./Allwmake -j 1",
+    #     ]
+
+    #     self.build_system.options = ["-v", "-n"]
+
+    # @sanity_function
+    # def validate_compile(self):
+    #     """Validate compilation by checking existance of binary"""
+    #     return sn.assert_eq(0, 0)
