79dadb79149b18ea6d1548f2ebbae0d8e58afa1c
Eleanor Broadway <eleanor@EPCC-MBP-P12-245.local>
2025-10-30 13:16:27 +0000

    Debugging cp source code

--- tests/apps/openfoam/openfoam_org_build.py
+++ tests/apps/openfoam/openfoam_org_build.py
@@ -65,12 +65,20 @@ class CompileOpenFOAM(rfm.CompileOnlyRegressionTest):
         self.prerun_cmds = [
             f"cp {self.openfoam_src.stagedir}/OpenFOAM-*.tar.gz .",
             f"cp {self.openfoam_src.stagedir}/ThirdParty-*.tar.gz ."
+            f"tar xf OpenFOAM-{OpenFOAMBase.openfoam_org_version_major}-{OpenFOAMBase.openfoam_org_version_patch}.tar.gz",
+            f"tar xf ThirdParty-{OpenFOAMBase.openfoam_org_version_major}-version-{OpenFOAMBase.openfoam_org_version_major}.tar.gz", 
+            f"mv OpenFOAM-{OpenFOAMBase.openfoam_org_version_major}-{OpenFOAMBase.openfoam_org_version_patch} OpenFOAM-{OpenFOAMBase.openfoam_org_version_major}",
+            f"mv ThirdParty-{OpenFOAMBase.openfoam_org_version_major}-version-{OpenFOAMBase.openfoam_org_version_major} ThirdParty-{OpenFOAMBase.openfoam_org_version_major}"
+            f"export FOAM_INST_DIR=`pwd`", 
+            f"export FOAM_SRC=${FOAM_INST_DIR}/OpenFOAM-{OpenFOAMBase.openfoam_org_version_major}",
+            f"export FOAM_THIRDPARTY=${FOAM_INST_DIR}/ThirdParty-{OpenFOAMBase.openfoam_org_version_major}",
+            f"printf "Install OpenFOAM in FOAM_INST_DIR: %s\n" ${FOAM_INST_DIR}"
         ]
 
-        self.build_system.commands = [
-            'chmod u+x site/compile.sh',
-            './site/compile.sh',
-        ]
+        # self.build_system.commands = [
+        #     'chmod u+x site/compile.sh',
+        #     './site/compile.sh',
+        # ]
 
 
 
